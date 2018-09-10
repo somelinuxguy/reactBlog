e48ca55ef33ef017ebe4101ea9576c92a6fdbadc
SomeLinuxGuy <github@sect.net>
2018-09-10 14:50:18 -0400

    learning React today.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Let's learn React
+I often deal with Wordpress or other such content delivering APIs, so I thought it would be fun to learn React and see how I would go about barfing the blog list returned from Wordpress, on to a page.
+
+First iteration is to just use a static blog list, to get the page working "right" before I then add in some fetch() code to try nabbing it all from Wordpress, then unraveling their JSON response.
+
+Just to prove a point: You don't need Wordpress to use Wordpress.
+
+You'll figure out what that means.
---
+++ main.js
@@ -0,0 +1,43 @@
+// pretend I got these from a wordpress API
+const posts = [{
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },{
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },{
+    "userId": 2,
+    "id": 3,
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  },{
+    "userId": 2,
+    "id": 4,
+    "title": "eum et est occaecati",
+    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+  }];
+
+let h = React.createElement;
+
+// the old 'fat arrow without braces is a RETURN' design pattern.
+let postList = posts.map(post => 
+    h('li',{className: "blogitem"}, [
+        h('p',{},`Title: ${post.title}`),
+        h('p',{},`User: ${post.userId}`),
+        h('p',{},`Body: ${post.body}`),
+        h('hr', {}, null)
+    ])
+)
+
+// note the children of ul is a function
+let myVDOM = h('div', {className: "postlist"}, [
+            h('h1',{}, 'Sic gorgiamus allos subjectatos nunc!'),
+            h('ul',{}, postList)
+            ]);
+
+// jam it on the page
+ReactDOM.render(myVDOM, document.getElementById('react-main'));
