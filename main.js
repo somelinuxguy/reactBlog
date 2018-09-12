043b311571b130185a8eb7d937f9f56d15c87048
SomeLinuxGuy <github@sect.net>
2018-09-12 14:15:34 -0400

    further react-iziation of this little Blog rendering project

--- main.js
+++ main.js
@@ -1,44 +1,47 @@
-// pretend I got these from a wordpress API
-let posts = [{
+// a constant, a list of posts from some given source.
+const posts = [{
     "userId": 1,
     "id": 1,
     "title": "sunt aut facere repellat provident occaecati",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
   },{
     "userId": 1,
     "id": 2,
     "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    "body": "est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla"
   },{
     "userId": 2,
-    "id": 3,
+    "id": 9,
     "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    "body": "et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut"
   },{
     "userId": 2,
     "id": 4,
     "title": "eum et est occaecati",
-    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    "body": "ullam et saepe reiciendis voluptatem adipisci sit amet autem assumenda provident rerum culpa quis hic commodi nesciunt rem tenetur doloremque ipsam iure quis sunt voluptatem rerum illo velit"
   }];
 
+// h is conventional, and shorter to type.
 const h = React.createElement;
 
-// This is a COMPONENT
-// It takes ONE thing and returns ONE thing
+// This is a COMPONENT: It takes ONE thing and returns ONE thing
 // It is being called by the component PostList.
-// let's add a button, and make it delete posts then redraw the page
-let BlogPost = post =>
+// Let's add buttons. These will run functions in our props.
+let BlogPost = props =>
     h('li',{className: "blogitem"}, [
-        h('p',{},`Title: ${post.title}`),
-        h('p',{},`User: ${post.userId}`),
-        h('p',{},`Body: ${post.body}`),
+        h('p',{},`Title: ${props.post.title}`),
+        h('p',{},`User: ${props.post.userId}`),
+        h('p',{},`Body: ${props.post.body}`),
         h('button',{
             onClick: () => {
-                console.log(`Deleting post: ${post.id}`);
-                posts = posts.filter(myPost => myPost.id !== post.id);
-                rerender();
+                props.deletepost(props.post.id);
             }
         },'delete'),
+        h('button',{
+            onClick: () => {
+                props.snakepost(props.post);
+            }
+        },'snake me'),
         h('hr', {}, null)
 ]);
 
@@ -46,27 +49,46 @@ let BlogPost = post =>
 // pay attention to the .map because we have a function, with a function in it.
 let PostList = (props) => {
     return h('ul', {}, 
-        props.blogEntries.map(post => 
-            h(BlogPost,post)
+        props.posts.map(post => 
+            h(BlogPost,{ snakepost: props.snakepost, deletepost: props.deletepost, post: post})
         )
     )
 }
 
-// now we are rendering a function named PostList, with the BlogEntries
-// since the object BlogEntries is an array, we'll wrap it in  {} and
-//  use .map in the PostList function
-let myVDOM = () =>
-    h('div', {className: "postlist"}, [
-        h('h1',{}, 'Sic gorgiamus allos subjectatos nunc!'),
-        h(PostList, { blogEntries: posts })
-    ]);
-
-// added some spaces to show more clearly that I am
-// invoking the component myVDOM
-let rerender = () => {
-    ReactDOM.render( h(myVDOM), document.getElementById('react-main') );
+// let us make a class instead.
+// all my manipulations are now done to the state (a magic thing)
+// render is a function, and it has plenty of functions in it
+// which are now gone from "above"
+class Homepage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            posts: this.props.posts 
+        };
+    }
+    render () {
+        let deletePost = (id) => {
+            this.setState( 
+                {posts : this.state.posts.filter(myPost => myPost.id !== id)}
+            );
+        }
+        let snakePost = (post) => {
+            let newPosts = this.state.posts.map(currentPost =>
+                (currentPost.id === post.id) ? 
+                    Object.assign({}, currentPost, {title : currentPost.title + 's'})
+                : currentPost
+            );
+            this.setState(
+                {posts : newPosts}
+            );
+        }
+        return h('div', {className: "postlist"}, [
+                h('h1',{}, 'Sic gorgiamus allos subjectatos nunc!'),
+                h(PostList, {snakepost: snakePost, deletepost: deletePost, posts: this.state.posts} )
+        ]);
+    }
 }
-
-// --- MAIN ---
-// Render the page for the first time.
-rerender();
+//
+// My main
+//
+ReactDOM.render( h(Homepage, {posts}), document.getElementById('react-main') );
